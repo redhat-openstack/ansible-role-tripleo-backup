29b6f5c83baee0256a9c540c097eb03733d5d4e4
Mathieu Bultel <mbultel@redhat.com>
2016-07-12 11:10:36 +0200

    Add ansible file and script for being able to execute the role by itself
    Change-Id: I0f20e17da4a0d38439a4fc3a37655cd925313508

---
+++ snap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WORKDIR=$1
+ACTION=$2
+VIRTHOST=$3
+# create virtual env
+virtualenv $WORKDIR
+. $WORKDIR/bin/activate
+# install role and deps
+python setup.py install
+pip install --no-cache-dir -r requirements.txt
+export $VIRTHOST
+# launch ansible
+ansible-playbook -i hosts -vvvv playbooks/$ACTION.yml \
+    --extra-vars local_working_dir=$WORKDIR\
+    --extra-vars undercloud_ip='\$UC_IP' \
+    --extra-vars virthost='\$VIRTHOST'
