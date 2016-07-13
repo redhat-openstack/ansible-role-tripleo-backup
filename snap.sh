afaa3554c41dab99ed822c5c68b4ebeba9321e9b
Mathieu Bultel <mbultel@redhat.com>
2016-07-13 13:36:21 +0200

    get undercloud ip, fetch ansible file and make sanity checks
    Change-Id: Ife669cd738c448ae6a4c03d90df92d1150ceb576

--- snap.sh
+++ snap.sh
@@ -3,7 +3,7 @@ WORKDIR=$1
 ACTION=$2
 VIRTHOST=$3
 # create virtual env
-virtualenv $WORKDIR
+virtualenv --system-site-packages $WORKDIR
 . $WORKDIR/bin/activate
 # install role and deps
 python setup.py install
@@ -15,7 +15,11 @@ $VIRTHOST ansible_host=$VIRTHOST ansible_user=root ansible_private_key_file=~/.s
 $VIRTHOST
 EOF
 # launch ansible
-ansible-playbook -i hosts -vvvv playbooks/$ACTION.yml \
-    --extra-vars local_working_dir=$WORKDIR\
-    --extra-vars undercloud_ip='\$UC_IP' \
-    --extra-vars virthost='\$VIRTHOST'
+if [ $ACTION == 'restore' ]; then
+    ansible-playbook -i hosts -vvvv playbooks/$ACTION.yml \
+        --extra-vars local_working_dir=$WORKDIR \
+        --extra-vars virthost=$VIRTHOST
+else
+    ansible-playbook -i hosts -vvvv playbooks/$ACTION.yml \
+        --extra-vars local_working_dir=$WORKDIR
+fi
