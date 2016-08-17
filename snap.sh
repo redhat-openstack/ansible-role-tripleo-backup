4de9ff24bb5857db3e05443ed2159ca5abd96cdf
Mathieu Bultel <mbultel@redhat.com>
2016-08-17 11:45:41 +0200

    Make http server url as parameter for snap.sh
    Change-Id: I9a3cc870225da640ce45c07327fae512c8c722af

--- snap.sh
+++ snap.sh
@@ -2,6 +2,8 @@
 WORKDIR=$1
 ACTION=$2
 VIRTHOST=$3
+HTTP_SRV=${4:-"artifacts.ci.centos.org"}
+HTTP_URL=${5:-"artifacts/rdo/images/liberty/ooo-snap"}
 # create virtual env
 virtualenv --system-site-packages $WORKDIR
 . $WORKDIR/bin/activate
@@ -28,7 +30,9 @@ export ANSIBLE_SSH_ARGS=' -F ssh.config.ansible'
 if [ $ACTION == 'restore' ]; then
     ansible-playbook -i hosts -vvvv playbooks/$ACTION.yml \
         --extra-vars local_working_dir=$WORKDIR \
-        --extra-vars virthost=$VIRTHOST
+        --extra-vars virthost=$VIRTHOST \
+        --extra-vars http_server=$HTTP_SRV \
+        --extra-vars http_path=$HTTP_URL
     ansible-playbook -i hosts -vvvv playbooks/sanity.yml \
         --extra-vars local_working_dir=$WORKDIR
 else
